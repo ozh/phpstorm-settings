ab533caa296a1ce91bad88ab330fd7e712c3eb04
Ozh <ozh@ozh.org>
2022-04-02 11:25:30 +0200

    PS-2021.1.4 Create ui.lnf.xml, wsl.distributions.xml, php.xml, YOURLS plugin file.php, colors.scheme.xml, laf.xml, databaseDrivers.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/YOURLS plugin file.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: ${PLUGIN_NAME}
+ * Plugin URI: https://github.com/your_name/your_plugin
+ * Description: One line description of your plugin
+ * Version: 1.0
+ * Author: Ozh
+ * Author URI: https://ozh.org/
+ */
+
+// No direct call
+if( !defined( 'YOURLS_ABSPATH' ) ) die();
+
